e098a1b407014c04f4a60f84f65c2dc5dfdc63b2
Jakub Neumann <4jakub.neumann7@gmail.com>
2023-01-25 10:07:48 +0100

    [structure] changed structure of chords

--- src/constants/chords.ts
+++ src/constants/chords.ts
@@ -1,32 +1,81 @@
 export type Note = [number, number | "x"];
 
-export type Chord = {
+export type Vexchord = {
   chord: Note[];
+  position?: number;
+}
+
+export type ChordVariation = {
+  id: string;
+  name: string;
+  vexchord: Vexchord;
 };
 
+export type Chord = ChordVariation[]
+
 export type Chords = {
-  [index: string]: Chord;
+  [chord: string]: Chord;
 };
 
 export const chords: Chords = {
-  dm: {
-    chord: [
-      [1, 2],
-      [2, 1],
-      [3, 2],
-      [4, 0],
-      [5, "x"],
-      [6, "x"],
-    ],
-  },
-  cm: {
-    chord: [
-      [1, 0],
-      [2, 1],
-      [3, 0],
-      [4, 2],
-      [5, 3],
-      [6, "x"],
-    ],
-  },
+  cm: [
+    {
+      id: 'asd',
+      name: 'C Major',
+      vexchord: {
+        chord: [
+          [1, 2],
+          [2, 1],
+          [3, 2],
+          [4, 0],
+          [5, "x"],
+          [6, "x"],
+        ],
+      },
+    },
+    {
+      id: 'bsd',
+      name: 'C Major second',
+      vexchord: {
+        chord: [
+          [1, 0],
+          [2, 1],
+          [3, 0],
+          [4, 2],
+          [5, 3],
+          [6, "x"],
+        ],
+      },
+    }
+  ],
+  dm: [
+    {
+      id: 'ddd',
+      name: 'C Major',
+      vexchord: {
+        chord: [
+          [1, 2],
+          [2, 1],
+          [3, 2],
+          [4, 0],
+          [5, "x"],
+          [6, "x"],
+        ],
+      },
+    },
+    {
+      id: 'fff',
+      name: 'C Major second',
+      vexchord: {
+        chord: [
+          [1, 0],
+          [2, 1],
+          [3, 0],
+          [4, 2],
+          [5, 3],
+          [6, "x"],
+        ],
+      },
+    }
+  ],
 };
